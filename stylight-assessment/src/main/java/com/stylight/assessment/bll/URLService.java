6004ed59dcbcc0ac098a8ffbf1118f62b7cfcf4b
Jahangir Alam <ja.dakghor@gmail.com>
2021-10-23 18:44:41 +0600

    building new url
    Signed-off-by: Jahangir Alam <ja.dakghor@gmail.com>

--- stylight-assessment/src/main/java/com/stylight/assessment/bll/URLService.java
+++ stylight-assessment/src/main/java/com/stylight/assessment/bll/URLService.java
@@ -1,6 +1,9 @@
 package com.stylight.assessment.bll;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -9,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import com.stylight.assessment.caching.URLCacheAgent;
 import com.stylight.assessment.dao.URLRepository;
+import com.stylight.assessment.model.Dummy;
 
 @Service
 public class URLService {
@@ -43,7 +47,7 @@ public class URLService {
 	private String makeURL(String url) {
 		String prettyUrl = "";
 
-		String[] split = url.split("?");
+		String[] split = url.split("\\?");
 		if (urlRepository.getUglyToPretty().containsKey(split[0])) {
 			prettyUrl = urlRepository.getUglyToPretty().get(split[0]);
 		} else {
@@ -54,11 +58,46 @@ public class URLService {
 		
 		String[] secondSplit = split[1].split("&");
 		
-		for (String string : secondSplit) {
+		int i;
+		LinkedList<Dummy> dummyList = new LinkedList<Dummy>();		
+		StringBuffer sb = new StringBuffer(split[0]);
+		sb.append("?");
+		
+		for (i = 0; i < secondSplit.length; i++) {
+			sb.append(secondSplit[i]);
+			if(urlRepository.getUglyToPretty().containsKey(sb.toString())) {
+				prettyUrl = urlRepository.getUglyToPretty().get(sb.toString());
+				dummyList.addFirst(new Dummy(secondSplit[i], true));
+			} else {
+				dummyList.addFirst(new Dummy(secondSplit[i], false));
+			}
 			
+			if(i < secondSplit.length - 1)
+				sb.append("&");
 		}
+		
+		sb = new StringBuffer(prettyUrl);
+		sb.append("?");
+//		for(; i < secondSplit.length; i++) {
+//			sb.append(secondSplit[i]);
+//			if(i < secondSplit.length - 1)
+//				sb.append("&");
+//		}
+		StringBuffer params = new StringBuffer("");
+		while(!dummyList.isEmpty()) {
+			Dummy dummy = dummyList.poll();
+			if(!dummy.isFind()) {
+				params.insert(0, dummy.getParam()+"&");
+			} else {
+				break;
+			}
+		}
+		sb.append(params.substring(0, params.length()-1));
+		
+		//caching
+		cacheAgent.setToCache(url, sb.toString());
 
-		return prettyUrl;
+		return sb.toString();
 	}
 
 	public Map<String, String> getPrettyToUglyMap(List<String> listPrettyUrl) {
---
+++ stylight-assessment/src/main/java/com/stylight/assessment/model/Dummy.java
@@ -0,0 +1,32 @@
+package com.stylight.assessment.model;
+
+public class Dummy {
+	String param;
+	boolean find;
+
+	public Dummy() {
+		super();
+	}
+
+	public Dummy(String param, boolean find) {
+		super();
+		this.param = param;
+		this.find = find;
+	}
+
+	public String getParam() {
+		return param;
+	}
+
+	public void setParam(String param) {
+		this.param = param;
+	}
+
+	public boolean isFind() {
+		return find;
+	}
+
+	public void setFind(boolean find) {
+		this.find = find;
+	}
+}
